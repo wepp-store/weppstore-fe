19acea396063f42845ea610fcdd7499cc9a93b7d
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-04 03:39:16 +0900

    feat: footer layout

---
+++ src/layouts/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>&copy; 2024 My PWA App Store</p>
+    </footer>
+  );
+};
+
+export default Footer;
