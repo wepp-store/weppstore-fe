778ddac430b5d752b69042861b1a17ca332e2605
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-05 02:11:47 +0900

    feat: apply wepp api

--- src/sections/main/MainSection.tsx
+++ src/sections/main/MainSection.tsx
@@ -1,7 +1,22 @@
+'use client';
+
+import { useWeppList } from '@/_apis/queries/wepp';
 import { Card } from '@/components/card';
 import { Section } from '@/components/section';
+import React from 'react';
 
 const MainSection = () => {
+  const {
+    data,
+    isLoading,
+    isFetched,
+    hasNextPage,
+    isFetchingNextPage,
+    fetchNextPage,
+  } = useWeppList();
+
+  const isNotData = !isLoading && !data?.pages[0]?.data?.length;
+
   return (
     <Section>
       <h2
@@ -22,18 +37,18 @@ const MainSection = () => {
         md:grid-cols-3
       "
       >
-        <Card>
-          <h3 className="text-xl font-semibold">App1</h3>
-          <p className="text-gray-700">This is a description for App 1</p>
-        </Card>
-        <Card>
-          <h3 className="text-xl font-semibold">App2</h3>
-          <p className="text-gray-700">This is a description for App 2</p>
-        </Card>
-        <Card>
-          <h3 className="text-xl font-semibold">App2</h3>
-          <p className="text-gray-700">This is a description for App 2</p>
-        </Card>
+        {isNotData && <>등록된 앱이 없습니다.</>}
+        {isFetched &&
+          data?.pages.map((group, i) => (
+            <React.Fragment key={i}>
+              {group.data.map((wepp) => (
+                <Card key={wepp.id}>
+                  <h3 className="text-xl font-semibold">{wepp.name}</h3>
+                  <p className="text-gray-700">{wepp.description}</p>
+                </Card>
+              ))}
+            </React.Fragment>
+          ))}
       </div>
     </Section>
   );
