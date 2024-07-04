c55d822a03e5c0bfd29ad7beb7f6fab997a34a87
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-05 00:56:26 +0900

    feat: api path

---
+++ src/_apis/path.ts
@@ -0,0 +1,14 @@
+export const PATH_API = {
+  // auth
+  AUTH: {
+    ME: '/auth/me',
+    SIGN_IN: '/auth/sign-in',
+    SIGN_UP: '/auth/sign-up',
+    REFRESH_TOKEN: '/auth/refresh',
+  },
+};
+
+/**
+- /oauth/{provider}/sign-in-uri
+- /oauth/{provider}/sign-in
+ */
