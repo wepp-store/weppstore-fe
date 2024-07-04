88b1a6db402989ce0feb84e70a30fdbcd79bcebb
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-05 00:59:54 +0900

    feat: sign in page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import { SignInFormSection } from '@/sections/sign-in';
+import React from 'react';
+
+const SignIn = () => {
+  return (
+    <div>
+      <SignInFormSection />
+    </div>
+  );
+};
+
+export default SignIn;
