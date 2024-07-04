77cb0963d8e88e1d5d5c6db0fcf2a9e41c1d442f
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-05 01:00:34 +0900

    feat: section component

--- src/components/section/Section.tsx
+++ src/components/section/Section.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './styles.module.scss';
-import { bindClassNames } from '@/utils/styles';
+import { bindClassNames } from '@/_utils/styles';
 
 type Props = React.HTMLAttributes<HTMLDivElement>;
 
