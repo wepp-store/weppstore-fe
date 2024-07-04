80cccca75104a01bfa6712e6404d50a39195c94f
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-05 01:01:44 +0900

    temp

--- src/utils/styles.ts
+++
@@ -1,5 +0,0 @@
-export const bindClassNames =
-  (styles: { [key: string]: string }) =>
-  (...classNames: string[]) => {
-    return classNames.map((className) => styles[className]).join(' ');
-  };
