82b83e2abcfe42b2935150539cea2ce4d0febad7
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-05 15:12:05 +0900

    feat: popover component

---
+++ src/components/popover/Popover.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import styles from './styles.module.scss';
+import { bindClassNames } from '@/_utils';
+import { AnimatePresence, motion } from 'framer-motion';
+
+const cx = bindClassNames(styles);
+
+interface PopoverProps {
+  children: React.ReactNode;
+  open: boolean;
+  onClose: () => void;
+  className?: string;
+}
+
+const Popover = ({ open, onClose, children, className = '' }: PopoverProps) => {
+  const popoverRef = React.useRef<HTMLDivElement>(null);
+
+  React.useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        popoverRef.current &&
+        !popoverRef.current.contains(event.target as Node)
+      ) {
+        onClose();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <AnimatePresence>
+      {open && (
+        <motion.div
+          ref={popoverRef}
+          className={`${cx('popover-content')} ${className}`}
+          initial={{ scale: 0.7, opacity: 0, y: 50 }}
+          animate={{ scale: 1, opacity: 1, y: 0 }}
+          exit={{ scale: 0.7, opacity: 0 }}
+          transition={{ duration: 0.1 }}
+        >
+          {children}
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default Popover;
