a6d0d1d4844cff61ec26e9142cd32598cdd76557
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-05 23:01:34 +0900

    feat: export wepp api path

--- src/_apis/queries/wepp/index.ts
+++ src/_apis/queries/wepp/index.ts
@@ -1,2 +1,6 @@
 export * from './wepp-list';
+export * from './wepp-detail';
+export * from './update-wepp';
+export * from './create-wepp';
+export * from './upload-image';
 export * from './mine-wepp-list';
