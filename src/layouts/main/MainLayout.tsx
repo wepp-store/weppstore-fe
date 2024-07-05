0eb4b2fc2a6b8e4f0a4d06f6e2c66b6a48f852d5
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-05 16:06:06 +0900

    fix: main layout header style

--- src/layouts/main/MainLayout.tsx
+++ src/layouts/main/MainLayout.tsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import Header from './header/Header';
 import Footer from './footer/Footer';
-import AuthGuard from '@/_auth/AuthGuard';
 
 interface Props {
   children: React.ReactNode;
@@ -10,11 +9,11 @@ interface Props {
 
 const MainLayout: React.FC<Props> = ({ children }) => {
   return (
-    <AuthGuard>
+    <>
       <Header />
-      <main className="container mx-auto px-4 py-8">{children}</main>
+      <main className="container mx-auto px-4 py-8 mt-20">{children}</main>
       <Footer />
-    </AuthGuard>
+    </>
   );
 };
 
