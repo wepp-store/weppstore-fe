e0eafc4d47ab0e069421dee5aac27528106b08e7
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-05 16:06:06 +0900

    fix: main layout header style

--- src/layouts/main/header/Header.tsx
+++ src/layouts/main/header/Header.tsx
@@ -1,11 +1,16 @@
-import { SearchIcon, UserRound } from 'lucide-react';
+import { SearchIcon } from 'lucide-react';
 import Image from 'next/image';
 import React from 'react';
+import AccountPopover from './AccountPopover';
 
 const Header = () => {
   return (
     <header
       className="
+      fixed
+      top-0
+      start-0
+      w-full
       flex
       items-center
       justify-between
@@ -31,7 +36,7 @@ const Header = () => {
       "
       >
         <SearchIcon />
-        <UserRound />
+        <AccountPopover />
       </div>
     </header>
   );
