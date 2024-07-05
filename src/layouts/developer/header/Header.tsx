d6c948f00047d93bbfc2b3f1f5aa468115c84943
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-05 23:05:05 +0900

    feat: developer layout header, footer

---
+++ src/layouts/developer/header/Header.tsx
@@ -0,0 +1,45 @@
+import { SearchIcon } from 'lucide-react';
+import Image from 'next/image';
+import React from 'react';
+import AccountPopover from './AccountPopover';
+
+const Header = () => {
+  return (
+    <header
+      className="
+      fixed
+      top-0
+      start-0
+      w-full
+      flex
+      items-center
+      justify-between
+      p-4
+      shadow-md
+    "
+    >
+      <div
+        className="
+        flex
+        items-center
+        gap-2
+      "
+      >
+        <Image src="/logo.svg" alt="wepp store logo" width={48} height={48} />
+        <h1 className="text-xl font-semibold">Wepp Developer</h1>
+      </div>
+      <div
+        className="
+        flex
+        items-center
+        gap-4
+      "
+      >
+        <SearchIcon />
+        <AccountPopover />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
