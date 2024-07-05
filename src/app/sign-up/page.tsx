d845764051132448bca266a2b3693db9617924e6
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-05 23:03:18 +0900

    feat: sign up page

---
+++ src/app/sign-up/page.tsx
@@ -0,0 +1,12 @@
+import { SignUpForm } from '@/sections/sign-up';
+import React from 'react';
+
+const SignUpPage = () => {
+  return (
+    <>
+      <SignUpForm />
+    </>
+  );
+};
+
+export default SignUpPage;
