a13fecde8a961b925d37a5cfa268f81a330172c0
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-05 23:05:32 +0900

    feat: Add WeppDashboardInfo, WeppDashboardHeader, WeppDashboardReviews, WeppInfoForm, and WeppInfoHeader components

---
+++ src/sections/developer/wepp/index.ts
@@ -0,0 +1,8 @@
+// wepp dashboard
+export { default as WeppDashboardInfo } from './WeppDashboardInfo';
+export { default as WeppDashboardHeader } from './WeppDashboardHeader';
+export { default as WeppDashboardReviews } from './WeppDashboardReviews';
+
+// wepp info
+export { default as WeppInfoForm } from './info/WeppInfoForm';
+export { default as WeppInfoHeader } from './info/WeppInfoHeader';
