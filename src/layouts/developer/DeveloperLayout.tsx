c9970b5f7ebcf7a589fd81a47e0c07b9efbf301a
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-05 23:04:51 +0900

    feat: developer layout component

--- src/layouts/developer/DeveloperLayout.tsx
+++ src/layouts/developer/DeveloperLayout.tsx
@@ -1,13 +1,21 @@
 'use client';
 import { DeveloperGuard } from '@/_auth';
 import React from 'react';
+import Header from './header/Header';
+import Footer from './footer/Footer';
 
 interface Props {
   children: React.ReactNode;
 }
 
 const DeveloperLayout: React.FC<Props> = ({ children }) => {
-  return <DeveloperGuard>{children}</DeveloperGuard>;
+  return (
+    <DeveloperGuard>
+      <Header />
+      <main className="container mx-auto px-4 py-8 mt-20">{children}</main>
+      <Footer />
+    </DeveloperGuard>
+  );
 };
 
 export default DeveloperLayout;
