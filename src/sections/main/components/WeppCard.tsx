10cb8dd663902ef92d71ec34269cf68e3168030e
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-08 02:05:26 +0900

    feat: Update WeppCard component with link to individual wepp page

--- src/sections/main/components/WeppCard.tsx
+++ src/sections/main/components/WeppCard.tsx
@@ -1,30 +1,54 @@
 import { IWepp } from '@/_types';
 import { Card } from '@/components/card';
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const WeppCard = ({ wepp }: { wepp: IWepp }) => {
   const { name, description, screenshots, logo } = wepp;
 
   return (
-    <Card className="p-4">
-      <Image
-        src={logo}
-        alt="wepp icon"
-        width={48}
-        height={48}
-        onError={(e) => {
-          e.currentTarget.src = '/logo.svg';
-        }}
-        className="w-20 h-20 mx-auto mb-2 rounded-xl"
-      />
-      <h3 className="text-center text-sm text-gray-600">{name}</h3>
-      <p className="text-center text-sm text-gray-600">액션 · 4.5 ★</p>
-      <p className="text-center text-sm text-gray-600">{description}</p>
-      <button className="mt-2 w-full bg-blue-500 text-white px-4 py-2 rounded-lg">
-        받기
-      </button>
-    </Card>
+    <Link href={`/wepp/${wepp.id}`}>
+      <Card
+        className="
+        bg-white
+        rounded-lg
+        p-4
+        flex
+        flex-col
+        hover:shadow-lg
+      "
+      >
+        <Image
+          src={logo || ''}
+          alt="wepp icon"
+          width={48}
+          height={48}
+          onError={(e) => {
+            e.currentTarget.src = '/logo.svg';
+          }}
+          className="w-16 h-16 bg-gray-200 rounded-lg mb-2 self-center"
+        />
+        <h3 className="font-semibold text-sm mb-1">{name}</h3>
+        <p className="text-xs text-gray-500 mb-2">액션 · 4.5 ★</p>
+        <div className="flex items-center justify-between mt-auto">
+          <span className="text-xs font-semibold text-gray-500">카테고리</span>
+          <button
+            className="
+            bg-blue-500
+            text-white
+            text-xs
+            font-semibold
+            py-1
+            px-2
+            rounded
+          "
+          >
+            받기
+          </button>
+        </div>
+      </Card>
+    </Link>
   );
 };
 
