e7541b3a27f893e8eed4fc8b717f6b93bf7b8973
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-08 20:02:51 +0900

    feat: next ui provider

--- src/_providers/index.ts
+++ src/_providers/index.ts
@@ -1,2 +1,3 @@
+export { default as ClientNextUIProvider } from './ClientNextUIProvider';
 export { default as ClientNotistackProvider } from './ClientNotistackProvider';
 export { default as ClientQueryClientProvider } from './ClientQueryClientProvider';
