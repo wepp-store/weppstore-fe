1005ea69439b406c657983c52c09d556f79e8456
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-09 01:22:34 +0900

    feat: custom bread crumbs component

---
+++ src/components/custom-breadcrumbs/CustomBreadcrumbs.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Breadcrumbs, BreadcrumbItem } from '@nextui-org/react';
+import { usePathname, useRouter } from 'next/navigation';
+
+interface Props {
+  paths: { path: string; name: string }[];
+}
+
+export default function CustomBreadcrumbs({ paths }: Props) {
+  const pathname = usePathname();
+  const { push } = useRouter();
+
+  return (
+    <Breadcrumbs
+      underline="active"
+      onAction={(key) => push(key as string)}
+      className="mb-4"
+    >
+      {paths.map(({ path, name }) => (
+        <BreadcrumbItem key={path} isCurrent={pathname === path}>
+          {name}
+        </BreadcrumbItem>
+      ))}
+    </Breadcrumbs>
+  );
+}
---
+++ src/components/custom-breadcrumbs/index.ts
@@ -0,0 +1,1 @@
+export { default as CustomBreadcrumbs } from './CustomBreadcrumbs';
