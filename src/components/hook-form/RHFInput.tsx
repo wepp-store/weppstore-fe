1d2332efd992daf48ef94e6c776c030d073d324a
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-09 01:22:49 +0900

    feat: migration next-ui

--- src/components/hook-form/RHFInput.tsx
+++ src/components/hook-form/RHFInput.tsx
@@ -1,75 +1,27 @@
-import { useFormContext } from 'react-hook-form';
-import styles from './style.module.scss';
-import { bindClassNames, px } from '@/_utils';
-import { CircleX } from 'lucide-react';
+import { Controller, useFormContext } from 'react-hook-form';
+import { Input, InputProps } from '@nextui-org/react';
 
-// ----------------------------------------------------------------------
-const cx = bindClassNames(styles);
-// ----------------------------------------------------------------------
-
-interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
+interface Props extends InputProps {
   name: string;
-  label?: string;
-  required?: boolean;
-  readonly?: boolean;
   loading?: boolean;
-  unit?: string;
 }
 
-export default function RHFInput({
-  name,
-  label,
-  readonly = false,
-  required = false,
-  loading = false,
-  unit,
-  ...other
-}: Props) {
-  const {
-    register,
-    setValue,
-    watch,
-    formState: { errors },
-  } = useFormContext();
-
-  const isError = !!errors[name];
-
-  const errorMessage = (errors[name]?.message as string) || '';
-
-  const inputValue = watch(name);
-
-  const handleClear = () => {
-    setValue(name, '');
-  };
-
-  if (readonly) {
-    return (
-      <div className={cx('RHFInput')}>
-        <div>{watch(name)}</div>
-      </div>
-    );
-  }
+export default function RHFInput({ name, loading = false, ...other }: Props) {
+  const { control, setValue } = useFormContext();
 
   return (
-    <div className={cx('RHFInput')}>
-      {label && (
-        <label className={cx('label')} htmlFor={name}>
-          {label}
-          {required && <span className={cx('required')}>*</span>}
-        </label>
-      )}
-      <div className={cx('input')}>
-        <input
-          style={isError ? { border: `${px(1)} solid rgb(255, 43, 43)` } : {}}
-          {...register(name, { required })}
+    <Controller
+      name={name}
+      control={control}
+      render={({ field }) => (
+        <Input
+          isClearable
+          labelPlacement="outside"
+          onClear={() => setValue(name, '')}
+          {...field}
           {...other}
         />
-        {inputValue && (
-          <CircleX className={cx('closeIcon')} onClick={handleClear} />
-        )}
-      </div>
-
-      {isError && <p className={cx('helperText')}>{errorMessage}</p>}
-    </div>
+      )}
+    />
   );
 }
--- src/components/hook-form/RHFTextArea.tsx
+++ src/components/hook-form/RHFTextArea.tsx
@@ -1,75 +1,25 @@
 import { Controller, useFormContext } from 'react-hook-form';
-import styles from './style.module.scss';
 import React from 'react';
-import { bindClassNames, px } from '@/_utils';
+import { TextAreaProps, Textarea } from '@nextui-org/react';
 
-// ----------------------------------------------------------------------
-const cx = bindClassNames(styles);
-// ----------------------------------------------------------------------
-
-interface Props extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+interface Props extends TextAreaProps {
   name: string;
-  label?: string;
-  placeholder?: string;
-  required?: boolean;
-  readonly?: boolean;
   loading?: boolean;
-  unit?: string;
-  height?: string;
 }
 
 export default function RHFTextarea({
   name,
-  label,
-  placeholder,
-  readonly = false,
-  required = false,
   loading = false,
-  height = '100%',
-  unit,
   ...other
 }: Props) {
-  const handleResizeHeight = (element: HTMLTextAreaElement) => {
-    element.style.height = 'auto';
-    element.style.height = px(element.scrollHeight);
-  };
+  const { control } = useFormContext();
 
-  const { control, watch } = useFormContext();
-
-  if (readonly) {
-    return <div className={cx('RHFTextarea')}>{watch(name)}</div>;
-  }
   return (
     <Controller
       name={name}
       control={control}
-      rules={{ required: required && '필수입니다.' }}
-      render={({
-        field: { value, onChange, ref },
-        fieldState: { error },
-        ...props
-      }) => (
-        <div className={cx('RHFTextarea')}>
-          {label && (
-            <label className={cx('label')} htmlFor={name}>
-              {label}
-              {required && <span className={cx('label')}>*</span>}
-            </label>
-          )}
-          <textarea
-            style={{
-              border: error ? `${px(1)} solid rgb(255, 43, 43)` : '',
-              height, // 기본값 설정
-            }}
-            value={value}
-            onChange={onChange}
-            ref={ref}
-            placeholder={placeholder}
-            {...other}
-            {...props}
-            onInput={(e: any) => handleResizeHeight(e.target)}
-          />
-        </div>
+      render={({ field }) => (
+        <Textarea labelPlacement="outside" {...field} {...other} />
       )}
     />
   );
