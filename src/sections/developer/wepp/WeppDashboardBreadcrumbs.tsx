696743d3a87d0e9cb6aedfdab4d684f2d9d41566
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-09 03:09:33 +0900

    feat: developer dashboard breadcrumbs

---
+++ src/sections/developer/wepp/WeppDashboardBreadcrumbs.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { CustomBreadcrumbs } from '@/components/custom-breadcrumbs';
+import { useParams } from 'next/navigation';
+import React from 'react';
+
+const WeppDashboardBreadcrumbs = () => {
+  const { id }: { id: string } = useParams();
+
+  return (
+    <CustomBreadcrumbs
+      paths={[
+        { name: '홈', path: '/developer' },
+        { name: '앱 대시보드', path: `/developer/wepp/${id}` },
+      ]}
+    />
+  );
+};
+
+export default WeppDashboardBreadcrumbs;
