8f06029ddd7065d37b9ad1cb0e3db80d9c0df59b
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-09 01:54:56 +0900

    feat: layout styles

--- src/layouts/developer/DeveloperLayout.tsx
+++ src/layouts/developer/DeveloperLayout.tsx
@@ -12,7 +12,7 @@ const DeveloperLayout: React.FC<Props> = ({ children }) => {
   return (
     <DeveloperGuard>
       <Header />
-      <main className="container mx-auto p-4">{children}</main>
+      <main className="container mx-auto p-4 flex-grow">{children}</main>
       <Footer />
     </DeveloperGuard>
   );
