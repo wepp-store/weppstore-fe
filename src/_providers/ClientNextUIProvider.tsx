15027780ff28dd9e25bf47381692f4503707eabf
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-09 03:08:52 +0900

    feat: client routing provider

--- src/_providers/ClientNextUIProvider.tsx
+++ src/_providers/ClientNextUIProvider.tsx
@@ -2,14 +2,17 @@
 'use client';
 
 import { NextUIProvider } from '@nextui-org/react';
+import { useRouter } from 'next/navigation';
 
 export default function ClientNextUIProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const { push } = useRouter();
+
   return (
-    <NextUIProvider className="flex flex-col min-h-screen">
+    <NextUIProvider className="flex flex-col min-h-screen" navigate={push}>
       {children}
     </NextUIProvider>
   );
