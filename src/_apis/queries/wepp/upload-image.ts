92a2cc878b1ea55df9da1f5b5e879695e56327e7
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-09 01:21:47 +0900

    fix: image upload path

--- src/_apis/queries/wepp/upload-image.ts
+++ src/_apis/queries/wepp/upload-image.ts
@@ -4,21 +4,27 @@ import { UseMutationOptions, useMutation } from '@tanstack/react-query';
 import { useSnackbar } from 'notistack';
 import { PATH_API } from '../../path';
 import { axiosInstance } from '../../axios';
+import { useParams } from 'next/navigation';
 
 export const useUploadWeppImage = (
   options?: Omit<UseMutationOptions<any, any, File>, 'mutationKey'>
 ) => {
+  const { id: weppId }: { id: string } = useParams();
   const { enqueueSnackbar } = useSnackbar();
 
   return useMutation({
     mutationFn: async (file: File) => {
-      const response = await axiosInstance.post(PATH_API.WEPP.UPLOAD, file, {
-        headers: {
-          'Content-Type': 'application/octet-stream',
-          'Content-Disposition': `attachment; filename="${file.name}"`,
-        },
-      });
+      if (!weppId) throw new Error('weppId is required');
 
+      const response = await axiosInstance.post(
+        `${PATH_API.WEPP.UPLOAD}/${weppId}`,
+        file,
+        {
+          headers: {
+            'Content-Type': 'application/octet-stream',
+          },
+        }
+      );
       return response.data;
     },
     onError: (error) => {
