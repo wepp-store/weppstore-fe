493dea474ef22d7474d33f64039eddd6a203eca3
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-09 01:21:06 +0900

    feat: add aspect-ratio tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,13 @@ module.exports = {
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      aspectRatio: {
+        mobile: '9 / 16',
+        tablet: '3 / 4',
+        desktop: '16 / 9',
+      },
+    },
   },
   darkMode: 'class',
   plugins: [nextui()],
