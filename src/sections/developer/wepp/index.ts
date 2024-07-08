2c2a84d9481276c58d101059629a5cc72a0055a7
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-09 03:10:08 +0900

    .

--- src/sections/developer/wepp/index.ts
+++ src/sections/developer/wepp/index.ts
@@ -2,6 +2,7 @@
 export { default as WeppDashboardInfo } from './WeppDashboardInfo';
 export { default as WeppDashboardHeader } from './WeppDashboardHeader';
 export { default as WeppDashboardReviews } from './WeppDashboardReviews';
+export { default as WeppDashboardBreadcrumbs } from './WeppDashboardBreadcrumbs';
 
 // wepp info
 export { default as WeppInfoForm } from './info/WeppInfoForm';
