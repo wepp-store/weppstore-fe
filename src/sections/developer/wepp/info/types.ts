6111d5f15f040125fa98e6f2835e648e3111bc3f
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-09 03:10:29 +0900

    feat: add omit views in Wepp field interface

--- src/sections/developer/wepp/info/types.ts
+++ src/sections/developer/wepp/info/types.ts
@@ -2,5 +2,11 @@ import { ICategory, IWepp } from '@/_types';
 
 export type WeppField = Omit<
   IWepp,
-  'id' | 'developerId' | 'developer' | 'createdAt' | 'updatedAt' | 'categories'
+  | 'id'
+  | 'developerId'
+  | 'developer'
+  | 'createdAt'
+  | 'updatedAt'
+  | 'categories'
+  | 'views'
 > & { categories: Omit<ICategory, 'description'>[] };
