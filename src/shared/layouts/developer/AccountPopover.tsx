3972574cc2f2104a5cf86c26cf5f43f92253352e
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-09 21:46:00 +0900

    feat: sign out feature

--- src/shared/layouts/developer/AccountPopover.tsx
+++ src/shared/layouts/developer/AccountPopover.tsx
@@ -1,35 +1,42 @@
+import { useAuth, useSignOut } from '@/shared/apis/queries/auth';
 import { PATH } from '@/shared/constants';
 import {
   Avatar,
+  Button,
   Dropdown,
   DropdownItem,
   DropdownMenu,
   DropdownTrigger,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  useDisclosure,
 } from '@nextui-org/react';
 import { useRouter } from 'next/navigation';
 import { useSnackbar } from 'notistack';
 
 const AccountPopover = () => {
   const router = useRouter();
-  const { enqueueSnackbar } = useSnackbar();
 
-  const handleLogout = async () => {
-    try {
-      // logout();
-      router.replace(PATH.AUTH.SIGN_IN);
-    } catch (error) {
-      console.error(error);
-      enqueueSnackbar('Unable to logout!', { variant: 'error' });
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  const { data: me } = useAuth();
+
+  const signOutMutation = useSignOut();
+
+  const onAction = (key: React.Key) => {
+    if (key === 'settings') {
+      // router.push('');
+    } else if (key === 'logout') {
+      onOpen();
     }
   };
 
-  const handleClickItem = (path: string) => {
-    router.push(path);
-  };
-
   return (
-    <div className="relative">
-      <Dropdown placement="bottom-end">
+    <>
+      <Dropdown placement="bottom-end" backdrop="blur">
         <DropdownTrigger>
           <Avatar
             isBordered
@@ -41,23 +48,48 @@ const AccountPopover = () => {
             src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
           />
         </DropdownTrigger>
-        <DropdownMenu aria-label="Profile Actions" variant="flat">
+        <DropdownMenu
+          aria-label="Avatar Actions"
+          variant="flat"
+          onAction={onAction}
+        >
           <DropdownItem key="profile" className="h-14 gap-2">
             <p className="font-semibold">Signed in as</p>
-            <p className="font-semibold">zoey@example.com</p>
+            <p className="font-semibold">{me?.email}</p>
           </DropdownItem>
-          <DropdownItem key="settings">My Settings</DropdownItem>
-          <DropdownItem key="team_settings">Team Settings</DropdownItem>
-          <DropdownItem key="analytics">Analytics</DropdownItem>
-          <DropdownItem key="system">System</DropdownItem>
-          <DropdownItem key="configurations">Configurations</DropdownItem>
-          <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
+          <DropdownItem key="settings" showDivider>
+            My Settings
+          </DropdownItem>
           <DropdownItem key="logout" color="danger">
             Log Out
           </DropdownItem>
         </DropdownMenu>
       </Dropdown>
-    </div>
+
+      {/* sign out modal */}
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="justify-center mt-4">
+                정말 로그아웃하시겠습니까?
+              </ModalHeader>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  아니요
+                </Button>
+                <Button
+                  color="primary"
+                  onPress={() => signOutMutation.mutate()}
+                >
+                  네, 로그아웃할래요.
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 
