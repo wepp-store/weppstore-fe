d1a48a9c08850f843280280db342817016ed41e2
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-09 17:41:40 +0900

    .

--- src/features/update-wepp/ui/sections/index.ts
+++ src/features/update-wepp/ui/sections/index.ts
@@ -1,4 +1,5 @@
 export { default as UpdateWeppVersionSection } from './UpdateWeppVersionSection';
+export { default as UpdateWeppDevicesSection } from './UpdateWeppDevicesSection';
 export { default as UpdateWeppBasicInfoSection } from './UpdateWeppBasicInfoSection';
 export { default as UpdateWeppCategoriesSection } from './UpdateWeppCategoriesSection';
 export { default as UpdateWeppScreenshotsSection } from './UpdateWeppScreenshotsSection';
