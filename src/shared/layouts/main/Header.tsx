4bace6a2fbb3e27b6ada1e8ee0dfe17d403dec30
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-09 15:36:44 +0900

    feat: Update header navigation in main layout

--- src/shared/layouts/main/Header.tsx
+++ src/shared/layouts/main/Header.tsx
@@ -1,28 +1,48 @@
-import { Navbar, NavbarBrand, NavbarContent } from '@nextui-org/react';
+'use client';
+import {
+  Link,
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+} from '@nextui-org/react';
 import Image from 'next/image';
 import AccountPopover from './AccountPopover';
 import SearchInput from './SearchInput';
+import { usePathname } from 'next/navigation';
 
 const Header = () => {
+  const pathname = usePathname();
+
+  const isActive = (path: string) => {
+    return pathname === path ? 'page' : undefined;
+  };
+
   return (
     <Navbar isBordered maxWidth="full">
-      <NavbarBrand>
-        <Image src="/logo.svg" alt="wepp store logo" width={48} height={48} />
-        <h1 className="text-xl font-semibold">Wepp Store</h1>
-      </NavbarBrand>
+      <NavbarBrand className="gap-16">
+        <div className="flex gap-4 items-center">
+          <Image src="/logo.svg" alt="wepp store logo" width={48} height={48} />
+          <h1 className="text-xl font-semibold">Wepp Store</h1>
+        </div>
 
-      {/* <NavbarContent className="hidden sm:flex gap-4 grow" justify="center">
-        <NavbarItem isActive>
-          <Link href="#" aria-current="page">
-            Customers
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Integrations
-          </Link>
-        </NavbarItem>
-      </NavbarContent> */}
+        <NavbarContent className="hidden sm:flex gap-4">
+          <NavbarItem isActive>
+            <Link
+              href="/apps"
+              color="foreground"
+              aria-current={isActive('/apps')}
+            >
+              앱
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link href="/games" aria-current={isActive('/games')}>
+              게임
+            </Link>
+          </NavbarItem>
+        </NavbarContent>
+      </NavbarBrand>
 
       <NavbarContent justify="end">
         <SearchInput />
