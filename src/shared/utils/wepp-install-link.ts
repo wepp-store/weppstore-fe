843378f29136d6c1bab4cb4ac909641cfa6a1e67
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-09 20:23:32 +0900

    fix: add props to wepp install link

---
+++ src/shared/utils/wepp-install-link.ts
@@ -0,0 +1,8 @@
+export const installLink = (
+  name: string | undefined,
+  weppUrl: string | undefined,
+  logoUrl: string | undefined | null
+) => {
+  if (!weppUrl || !logoUrl) return '#';
+  return `${weppUrl}#wepp-install-modal-v${name}-v${logoUrl}`;
+};
