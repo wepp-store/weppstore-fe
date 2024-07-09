7df1e30d46158400cd1139440006d23ca233fb99
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-09 17:06:51 +0900

    fix: sync checked state

--- src/features/update-wepp/ui/sections/UpdateWeppCategoriesSection.tsx
+++ src/features/update-wepp/ui/sections/UpdateWeppCategoriesSection.tsx
@@ -11,14 +11,11 @@ const UpdateWeppCategoriesSection = () => {
 
   const { data } = useCategories();
 
+  const currentCategories = watch('categories');
+
   const onChange =
-    (category: Omit<ICategory, 'description'>) =>
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      const isChecked = e.target.checked;
-
-      const currentCategories = watch('categories');
-
-      if (isChecked) {
+    (category: Omit<ICategory, 'description'>) => (isSelected: boolean) => {
+      if (isSelected) {
         setValue('categories', [...currentCategories, category]);
       } else {
         setValue(
@@ -32,17 +29,17 @@ const UpdateWeppCategoriesSection = () => {
     <Section>
       <h2 className="text-xl font-semibold mb-4">카테고리</h2>
 
-      <CheckboxGroup orientation="horizontal" color="primary">
+      <div className="flex gap-4">
         {data?.map((category) => (
           <Checkbox
             key={category.id}
-            value={String(category.id)}
-            onChange={onChange(category)}
+            isSelected={currentCategories.some((c) => c.id === category.id)}
+            onValueChange={onChange(category)}
           >
             {category.name}
           </Checkbox>
         ))}
-      </CheckboxGroup>
+      </div>
     </Section>
   );
 };
