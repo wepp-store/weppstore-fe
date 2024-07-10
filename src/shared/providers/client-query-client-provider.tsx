5d9b8d47213347f776270d0e9080650f517ca2c6
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-10 17:37:45 +0900

    refactor: Update ClientQueryClientProvider to use lazy initialization for query client

--- src/shared/providers/client-query-client-provider.tsx
+++ src/shared/providers/client-query-client-provider.tsx
@@ -19,11 +19,11 @@ function makeQueryClient() {
         // above 0 to avoid refetching immediately on the client
         staleTime: 60 * 1000,
         refetchOnReconnect: true, // 네트워크 재연결이 발생한 경우
-        // refetchOnWindowFocus: false, // 브라우저에 포커스가 들어온 경우
-        // refetchOnMount: true, // 새로운 컴포넌트 마운트가 발생한 경우
+        refetchOnWindowFocus: false, // 브라우저에 포커스가 들어온 경우
+        refetchOnMount: true, // 새로운 컴포넌트 마운트가 발생한 경우
         // staleTime: 0, // 데이터가 fresh -> stale 되는 시간
         // gcTime: 0, // 캐시된 데이터가 얼마나 오랫동안 메모리에 유지될 것인지
-        // retry: 0,
+        retry: 0,
       },
     },
   });
