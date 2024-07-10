3fd8b5e687088529f47b62cf69577ce633472c5c
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-10 17:38:04 +0900

    feat: Add lazy loading for WeppDetailScreenshots

--- src/views/wepp-detail/ui/WeppDetailScreenshots.tsx
+++ src/views/wepp-detail/ui/WeppDetailScreenshots.tsx
@@ -1,6 +1,7 @@
 import { IWepp } from '@/shared/types';
+import { ImageGallery } from '@/shared/ui/image-gallery';
 import { Section } from '@/shared/ui/section';
-import { Image } from '@nextui-org/react';
+import { Image, useDisclosure } from '@nextui-org/react';
 import React from 'react';
 
 interface Props {
@@ -8,26 +9,37 @@ interface Props {
 }
 
 const WeppDetailScreenshots = ({ wepp }: Props) => {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
   const screenshots = wepp?.screenshots || [];
 
-  // TODO: order 기준 screenshot 정렬 로직 필요
   // TODO: device 기준 사진 다르게도 설정
 
   return (
-    <Section className="mb-6">
-      <h3 className="text-lg font-semibold mb-2">스크린샷</h3>
-      <div className="flex space-x-4 overflow-x-auto">
-        {screenshots.map((screenshot) => (
-          <Image
-            key={screenshot.id}
-            src={screenshot.url}
-            alt={`screenshot ${screenshot.order}`}
-            radius="lg"
-            className="flex-shrink-0 w-40 h-72 bg-gray-200"
-          />
-        ))}
-      </div>
-    </Section>
+    <>
+      <Section className="mb-6">
+        <h3 className="text-lg font-semibold mb-2">스크린샷</h3>
+        <div className="flex space-x-4 overflow-x-auto">
+          {screenshots.map((screenshot) => (
+            <Image
+              onClick={onOpen}
+              key={screenshot.id}
+              src={screenshot.url}
+              alt={`screenshot ${screenshot.order}`}
+              radius="lg"
+              className="flex-shrink-0 w-40 h-72 bg-gray-200"
+            />
+          ))}
+        </div>
+      </Section>
+
+      {/* screenshots gallery */}
+      <ImageGallery
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        images={screenshots.map((screenshot) => screenshot.url)}
+      />
+    </>
   );
 };
 
