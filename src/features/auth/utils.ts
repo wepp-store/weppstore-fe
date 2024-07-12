03ecaf465e1e55ad9e121f0bb99d5d1ba555a58a
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-13 00:16:27 +0900

    fix: export jwtDecode function in auth/utils.ts

--- src/features/auth/utils.ts
+++ src/features/auth/utils.ts
@@ -8,7 +8,7 @@ import { IToken } from './types';
 
 // ----------------------------------------------------------------------
 
-function jwtDecode(token: string) {
+export function jwtDecode(token: string) {
   const base64Url = token.split('.')[1];
   const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
   const jsonPayload = decodeURIComponent(
