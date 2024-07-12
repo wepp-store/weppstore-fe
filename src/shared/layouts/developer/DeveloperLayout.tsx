0e3e7083d703882b01b2b8c9c2a18383d5bc205d
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-13 01:53:54 +0900

    fix: layout styles

--- src/shared/layouts/developer/DeveloperLayout.tsx
+++ src/shared/layouts/developer/DeveloperLayout.tsx
@@ -12,7 +12,9 @@ const DeveloperLayout: React.FC<Props> = ({ children }) => {
   return (
     <DeveloperGuard>
       <Header />
-      <main className="container mx-auto p-4 flex-grow">{children}</main>
+      <main className="flex-grow p-4 sm:p-6 md:p-8 lg:p-10 xl:p-12">
+        {children}
+      </main>
       <Footer />
     </DeveloperGuard>
   );
