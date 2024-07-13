9678092af86177273bb699ebcf57cfc4f0c79fd9
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-14 01:33:00 +0900

    feat: wepp detail prefetch

--- src/app/developer/wepp/[id]/info/page.tsx
+++ src/app/developer/wepp/[id]/info/page.tsx
@@ -1,12 +1,26 @@
 import { UpdateWeppForm } from '@/features/update-wepp';
+import { weppDetailOptions } from '@/shared/apis/queries/wepp';
 import { WeppInfoBreadcrumbs } from '@/views/developer/wepp-info';
+import {
+  dehydrate,
+  QueryClient,
+  HydrationBoundary,
+} from '@tanstack/react-query';
 
-const Page = () => {
+const Page = async ({ params }: { params: { id: string } }) => {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery(
+    weppDetailOptions({
+      weppId: params.id,
+    })
+  );
+
   return (
-    <>
+    <HydrationBoundary state={dehydrate(queryClient)}>
       <WeppInfoBreadcrumbs />
       <UpdateWeppForm />
-    </>
+    </HydrationBoundary>
   );
 };
 
