6983680a035631492edfec3441255a96c39fcd6c
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-14 17:49:36 +0900

    feat: Add isInvalid prop to RHFInput component

--- src/shared/ui/hook-form/RHFInput.tsx
+++ src/shared/ui/hook-form/RHFInput.tsx
@@ -5,20 +5,27 @@ interface Props extends InputProps {
   name: string;
 }
 
-export default function RHFInput({ name, isReadOnly, ...other }: Props) {
+export default function RHFInput({
+  name,
+  isInvalid,
+  isReadOnly,
+  ...other
+}: Props) {
   const { control, setValue } = useFormContext();
 
   return (
     <Controller
       name={name}
       control={control}
-      render={({ field }) => (
+      render={({ field, fieldState: { error } }) => (
         <Input
           labelPlacement="outside"
           // TODO: isReadOnly일 때 clear 버튼이 보이지 않도록 nextui에 요청
           isClearable={!isReadOnly}
           onClear={!isReadOnly ? () => setValue(name, '') : undefined}
           isReadOnly={isReadOnly}
+          isInvalid={!!error || isInvalid}
+          errorMessage={error?.message}
           {...field}
           {...other}
         />
