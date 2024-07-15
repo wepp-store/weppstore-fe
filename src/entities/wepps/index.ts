d7463c9973e5434622ffb5e444e710032ebdf531
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-15 13:23:36 +0900

    feat: Add WeppComment component for displaying user comments

--- src/entities/wepps/index.ts
+++ src/entities/wepps/index.ts
@@ -1,2 +1,3 @@
 export * from './ui/devices-chip';
 export { default as WeppCard } from './ui/wepp-card';
+export { default as WeppComment } from './ui/wepp-comment';
---
+++ src/entities/wepps/ui/wepp-comment.tsx
@@ -0,0 +1,29 @@
+import { IComment } from '@/shared/types';
+import { StarRating } from '@/shared/ui/star-rating';
+import { User } from '@nextui-org/react';
+import React from 'react';
+
+interface Props {
+  comment: Pick<IComment, 'content' | 'rating' | 'user'>;
+}
+
+const WeppComment = ({ comment }: Props) => {
+  const { content, user } = comment;
+
+  return (
+    <div className="border-b border-gray-200 py-4">
+      <div className="flex items-center mb-2">
+        <User
+          name={user.userName}
+          description={user.email}
+          avatarProps={{
+            src: '/no-image.svg',
+          }}
+        />
+      </div>
+      <p className="text-gray-700">{content}</p>
+    </div>
+  );
+};
+
+export default WeppComment;
