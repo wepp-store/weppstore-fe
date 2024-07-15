c72db73acd2a1e09ea9cef508ad04ceb204b832f
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-16 05:13:40 +0900

    feat: Update user interface with additional fields

--- src/shared/types/user.ts
+++ src/shared/types/user.ts
@@ -1,11 +1,13 @@
 export interface IUser {
   id: number;
   email: string;
-  kind: 'DEVELOPER' | 'ADMIN';
+  kind: 'USER' | 'ADMIN';
   status: 'ACTIVE' | 'INACTIVE';
   userName: string;
   password: string;
   createdAt: Date;
   updatedAt: Date;
+  description: string;
+  profileUrl: string | null;
   // wepps: Wepp[];
 }
