f297d6a487d266c6f0c67b7b16591d428c9a4282
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-16 05:13:58 +0900

    feat: Improve sign-in page layout

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,22 @@
 import { LoginForm } from '@/features/auth';
 
 const SignIn = () => {
-  return <LoginForm />;
+  return (
+    <div
+      className="
+        fixed
+        w-full
+        h-full
+        top-0
+        left-0
+        flex justify-center items-center
+        bg-white
+        z-50
+      "
+    >
+      <LoginForm />
+    </div>
+  );
 };
 
 export default SignIn;
