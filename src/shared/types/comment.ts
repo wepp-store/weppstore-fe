e66540875edfd9c69078cfe16689bd68a2d5abfb
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-15 13:23:59 +0900

    feat: comment type

---
+++ src/shared/types/comment.ts
@@ -0,0 +1,14 @@
+import { IUser } from './user';
+import { IWepp } from './wepp';
+
+export interface IComment {
+  id: number;
+  content: string;
+  rating: number;
+  userId: number;
+  weppId: number;
+  user: IUser;
+  wepp: IWepp;
+  createdAt: string;
+  updatedAt: string;
+}
