b00eb1ce49db72308634683a44f3b1c6c49121e5
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-28 00:13:40 +0900

    feat: useDevice hook

---
+++ src/shared/hooks/use-device.ts
@@ -0,0 +1,21 @@
+const useDevice = () => {
+  const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
+
+  const isMobile =
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    );
+
+  const isTablet = /iPad/.test(navigator.userAgent);
+
+  const isDesktop = !isMobile && !isTablet;
+
+  return {
+    isIOS,
+    isMobile,
+    isTablet,
+    isDesktop,
+  };
+};
+
+export default useDevice;
