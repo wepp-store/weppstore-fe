ad8dedd533a1c5a4293d7ebbe9886bedec639f16
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-08-03 04:40:12 +0900

    feat: Add useGetHasLiked hook for checking if a wepp has been liked

---
+++ src/views/wepp-detail/api/get-has-liked.ts
@@ -0,0 +1,42 @@
+'use client';
+
+import { axiosInstance } from '@/shared/apis/axios';
+import { PATH_API } from '@/shared/apis/path';
+import { useAuth } from '@/shared/apis/queries/auth';
+import {
+  useQuery,
+  UseQueryResult,
+  UseQueryOptions,
+} from '@tanstack/react-query';
+import { AxiosError } from 'axios';
+import { useParams } from 'next/navigation';
+import { likeKeys } from './query-key-factory';
+
+type Props = Omit<UseQueryOptions, 'queryKey'>;
+type ResponseType = { hasLiked: boolean | undefined };
+
+export const useGetHasLiked = (params?: Props) => {
+  const { weppId }: { weppId: string } = useParams();
+  const { data: user } = useAuth();
+
+  return useQuery({
+    queryKey: likeKeys.hasLiked(weppId),
+    queryFn: async () => {
+      if (!user) {
+        return { hasLiked: undefined };
+      }
+
+      const response = await axiosInstance.get(PATH_API.LIKE.HAS_LIKED, {
+        params: {
+          wid: weppId,
+          uid: user.id,
+        },
+      });
+
+      return response.data;
+    },
+    gcTime: Infinity,
+    staleTime: Infinity,
+    ...params,
+  }) as UseQueryResult<ResponseType, AxiosError>;
+};
