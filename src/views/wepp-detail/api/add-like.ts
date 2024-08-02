3e82153a999d5d343b471e9448b7e56fcf622119
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-08-03 04:40:03 +0900

    feat: Add useAddWeppLike hook for liking a wepp

---
+++ src/views/wepp-detail/api/add-like.ts
@@ -0,0 +1,45 @@
+'use client';
+
+import {
+  useMutation,
+  useQueryClient,
+  UseMutationOptions,
+} from '@tanstack/react-query';
+import { axiosInstance } from '@/shared/apis/axios';
+import { PATH_API } from '@/shared/apis/path';
+import toast from 'react-hot-toast';
+import { useParams } from 'next/navigation';
+import { useAuth } from '@/shared/apis/queries/auth';
+import { likeKeys } from './query-key-factory';
+
+const useAddWeppLike = (
+  options?: Omit<UseMutationOptions<any, any, any>, 'mutationKey'>
+) => {
+  const { weppId }: { weppId: string } = useParams();
+  const { data: user } = useAuth();
+
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async () => {
+      if (!user) {
+        throw new Error('로그인이 필요합니다.');
+      }
+
+      const response = await axiosInstance.post(PATH_API.COMMENT.ROOT, {
+        data: {
+          weppId,
+          userId: user.id,
+        },
+      });
+
+      return response.data;
+    },
+    onSuccess: () => {
+      queryClient.setQueryData(likeKeys.hasLiked(weppId), { hasLiked: true });
+      toast.success('좋아요가 추가되었습니다.');
+    },
+  });
+};
+
+export default useAddWeppLike;
