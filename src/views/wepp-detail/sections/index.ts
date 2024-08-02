ef74fa011886a8fa2558a8af99d40d442b9e5d3f
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-08-03 04:40:30 +0900

    feat: Add WeppDetailSubInfo component to wepp detail sections

--- src/views/wepp-detail/sections/index.ts
+++ src/views/wepp-detail/sections/index.ts
@@ -1,6 +1,8 @@
 export { default as WeppDetailTitle } from './wepp-detail-title';
+export { default as WeppDetailSubInfo } from './wepp-detail-sub-info';
 export { default as WeppDetailComments } from './wepp-detail-comments';
 export { default as WeppDetailSimilars } from './wepp-detail-similars';
+export { default as WeppDetailDescription } from './wepp-detail-description';
 export { default as WeppDetailScreenshots } from './wepp-detail-screenshots';
 export { default as WeppDetailAdditionalInfo } from './wepp-detail-additional-info';
 export { default as WeppDetailUpdateHistories } from './wepp-detail-update-histories';
