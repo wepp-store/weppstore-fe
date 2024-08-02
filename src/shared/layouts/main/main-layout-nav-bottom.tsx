9f8e9da1413d2f0fc5634e24a6468cbd225fd388
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-08-02 20:52:10 +0900

    feat: Update main layout navigation to include "games" subpath

--- src/shared/layouts/main/main-layout-nav-bottom.tsx
+++ src/shared/layouts/main/main-layout-nav-bottom.tsx
@@ -5,13 +5,13 @@ import React from 'react';
 const MainLayoutNavBottom = () => {
   return (
     <div className="sticky bottom-0 left-0 z-50 w-full min-h-16 max-h-16 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600 sm:hidden">
-      <div className="grid h-full max-w-lg grid-cols-3 mx-auto">
+      <div className="grid h-full max-w-lg grid-cols-2 mx-auto">
         <Link href="/wepps" className="flex justify-center items-center">
           <LayoutGrid />
         </Link>
-        <Link href="/games" className="flex justify-center items-center">
+        {/* <Link href="/games" className="flex justify-center items-center">
           <Gamepad2 />
-        </Link>
+        </Link> */}
         <Link href="/profile" className="flex justify-center items-center">
           <User />
         </Link>
