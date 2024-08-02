66ab6a0f65038219a62f8e867a083a59fcc514cd
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-08-03 04:40:48 +0900

    feat: Add tooltips to device chips for compatibility information

--- src/views/wepp-detail/ui/devices-chip.tsx
+++ src/views/wepp-detail/ui/devices-chip.tsx
@@ -1,26 +1,32 @@
-import { Chip } from '@nextui-org/react';
+import { Chip, Tooltip } from '@nextui-org/react';
 import { Monitor, Smartphone, Tablet } from 'lucide-react';
 
 export const DesktopChip = () => {
   return (
-    <Chip className="py-2 px-3">
-      <Monitor />
-    </Chip>
+    <Tooltip content="데스크탑 호환">
+      {/* <Chip className="py-2 px-3"> */}
+      <Monitor size={16} className="text-gray-500" />
+      {/* </Chip> */}
+    </Tooltip>
   );
 };
 
 export const MobileChip = () => {
   return (
-    <Chip className="py-2 px-3">
-      <Smartphone />
-    </Chip>
+    <Tooltip content="모바일 호환">
+      {/* <Chip className="py-2 px-3"> */}
+      <Smartphone size={16} className="text-gray-500" />
+      {/* </Chip> */}
+    </Tooltip>
   );
 };
 
 export const TabletChip = () => {
   return (
-    <Chip className="py-2 px-3">
-      <Tablet />
-    </Chip>
+    <Tooltip content="태블릿 호환">
+      {/* <Chip className="py-2 px-3"> */}
+      <Tablet size={16} className="text-gray-500" />
+      {/* </Chip> */}
+    </Tooltip>
   );
 };
