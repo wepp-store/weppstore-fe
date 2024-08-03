78a5c108c877b279bca268dd2e7ca3424f91868e
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-08-03 23:46:20 +0900

    feat: Add debounce to like button click handlers

--- src/views/wepp-detail/ui/wepp-like-button.tsx
+++ src/views/wepp-detail/ui/wepp-like-button.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useAddWeppLike, useDeleteWeppLike, useGetHasLiked } from '../api';
 import { Heart } from 'lucide-react';
+import { debounce } from '@/shared/utils';
 
 const WeppLikeButton = () => {
   const { data } = useGetHasLiked();
@@ -11,17 +12,26 @@ const WeppLikeButton = () => {
 
   const hasLiked = data?.hasLiked;
 
+  const handleAddLike = debounce(addMutation.mutate, 250);
+
+  const handleDeleteLike = debounce(deleteMutation.mutate, 250);
+
   if (hasLiked === undefined) {
     return null;
   }
 
   if (hasLiked) {
     return (
-      <Heart color="#ff7a7a" fill="#ff7a7a" onClick={deleteMutation.mutate} />
+      <Heart
+        color="#ff7a7a"
+        role="button"
+        fill="#ff7a7a"
+        onClick={handleDeleteLike}
+      />
     );
   }
 
-  return <Heart color="#ff7a7a" onClick={addMutation.mutate} />;
+  return <Heart color="#ff7a7a" role="button" onClick={handleAddLike} />;
 };
 
 export default WeppLikeButton;
