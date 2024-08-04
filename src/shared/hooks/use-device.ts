dd0615436e4a83e7653d8f7ab59082f85b8e11a2
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-08-05 03:35:53 +0900

    fix: to client side useDevice func

--- src/shared/hooks/use-device.ts
+++ src/shared/hooks/use-device.ts
@@ -1,16 +1,27 @@
 'use client';
 
-const useDevice = () => {
-  const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
+import React from 'react';
 
-  const isMobile =
-    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-      navigator.userAgent
+const useDevice = () => {
+  const [isIOS, setIsIOS] = React.useState(false);
+  const [isMobile, setIsMobile] = React.useState(false);
+  const [isTablet, setIsTablet] = React.useState(false);
+  const [isDesktop, setIsDesktop] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsIOS(/iPad|iPhone|iPod/.test(navigator.userAgent));
+
+    setIsMobile(
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      )
     );
 
-  const isTablet = /iPad/.test(navigator.userAgent);
+    setIsTablet(/iPad/.test(navigator.userAgent));
 
-  const isDesktop = !isMobile && !isTablet;
+    setIsDesktop(!isMobile && !isTablet);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return {
     isIOS,
