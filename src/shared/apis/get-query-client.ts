f3d93dba6dc1f049017dcb438ffa022fe27f3af5
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-08-05 16:05:56 +0900

    fix: separating server, client query client

--- src/shared/apis/get-query-client.ts
+++ src/shared/apis/get-query-client.ts
@@ -3,9 +3,10 @@ import {
   QueryClient,
   defaultShouldDehydrateQuery,
 } from '@tanstack/react-query';
+import { cache } from 'react';
 
-export function makeQueryClient() {
-  return new QueryClient({
+const makeQueryClient = () =>
+  new QueryClient({
     defaultOptions: {
       queries: {
         // With SSR, we usually want to set some default staleTime
@@ -26,20 +27,12 @@ export function makeQueryClient() {
       },
     },
   });
-}
 
 let browserQueryClient: QueryClient | undefined = undefined;
 
-export function getQueryClient() {
-  if (isServer) {
-    // Server: always make a new query client
-    return makeQueryClient();
-  } else {
-    // Browser: make a new query client if we don't already have one
-    // This is very important, so we don't re-make a new client if React
-    // suspends during the initial render. This may not be needed if we
-    // have a suspense boundary BELOW the creation of the query client
-    if (!browserQueryClient) browserQueryClient = makeQueryClient();
-    return browserQueryClient;
-  }
+export const getServerQueryClient = cache(() => makeQueryClient());
+
+export function getBrowserQueryClient() {
+  if (!browserQueryClient) browserQueryClient = makeQueryClient();
+  return browserQueryClient;
 }
