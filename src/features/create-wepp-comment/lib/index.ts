14f143767925bbcdb8cd579f05089d531326371b
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-08-08 03:50:35 +0900

    feat: comment replay state store

---
+++ src/features/create-wepp-comment/lib/index.ts
@@ -0,0 +1,1 @@
+export * from './use-reply-state-store';
---
+++ src/features/create-wepp-comment/lib/use-reply-state-store.ts
@@ -0,0 +1,18 @@
+import { create } from 'zustand';
+
+interface ReplyComment {
+  writer: string;
+  id: number;
+}
+
+interface Store {
+  replyComment: ReplyComment | null;
+  setReplyComment: (replyComment: ReplyComment | null) => void;
+  clearReplyComment: () => void;
+}
+
+export const useReplyStateStore = create<Store>()((set) => ({
+  replyComment: null,
+  setReplyComment: (replyComment) => set({ replyComment }),
+  clearReplyComment: () => set({ replyComment: null }),
+}));
