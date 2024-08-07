570dc97c68ba6b8b97aa52fe4052b44a364df66a
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-08-08 03:53:18 +0900

    feat: Improve WeppComment component UI and functionality

--- src/views/wepp-detail/ui/wepp-comment.tsx
+++ src/views/wepp-detail/ui/wepp-comment.tsx
@@ -1,71 +1,102 @@
 import { IComment } from '@/shared/types';
-import { StarRating } from '@/shared/ui/star-rating';
-import {
-  Button,
-  Modal,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  useDisclosure,
-  User,
-} from '@nextui-org/react';
+import { Avatar } from '@nextui-org/react';
 import React from 'react';
-import { useDeleteWeppComment } from '../api';
 import { useSession } from '@/shared/apis/queries/auth';
+import { useWeppDetail } from '@/shared/apis/queries/wepp';
+import { useParams } from 'next/navigation';
+import { timeAgo } from '@/shared/utils';
+import { useReplyStateStore } from '@/features/create-wepp-comment/lib';
+import WeppCommentDeleteButton from './wepp-comment-delete-button';
+import WeppCommentReplies from './wepp-comment-replies';
 
 interface Props {
-  comment: Pick<IComment, 'id' | 'content' | 'user'>;
+  comment: IComment;
 }
 
 const WeppComment = ({ comment }: Props) => {
-  const { id: commentId, content, user } = comment;
+  const {
+    // comment data
+    user,
+    _count,
+    content,
+    parentId,
+    createdAt,
+    id: commentId,
+  } = comment;
 
   const { user: me } = useSession();
 
-  const { isOpen, onOpenChange, onClose } = useDisclosure();
+  const [isShowReply, setIsShowReply] = React.useState(false);
 
-  const { mutate, isPending } = useDeleteWeppComment();
+  // creator check
+  const { weppId }: { weppId: string } = useParams();
+  const { data: wepp } = useWeppDetail({ weppId, enabled: false });
+
+  // reply state
+  const setReplyComment = useReplyStateStore((state) => state.setReplyComment);
 
   const isMyComment = user.id === me?.id;
+  const isCreator = wepp?.developer?.id === me?.id;
+
+  const isDeletable = isCreator || isMyComment;
+
+  const hasChildren = _count?.children > 0;
 
   return (
     <>
-      <div className="border-b border-gray-200 py-4">
-        <div className="flex items-center mb-2 gap-4">
-          <User
+      <div className="pt-4 flex justify-between">
+        <div className="flex gap-4 grow">
+          <Avatar
             name={user.userName}
-            description={user.email}
-            avatarProps={{
-              src: user.profileUrl || '/no-image.svg',
-            }}
+            src={user.profileUrl || '/no-image.svg'}
           />
-          {isMyComment && (
-            <Button size="sm" variant="light" onPress={onOpenChange}>
-              삭제
-            </Button>
-          )}
-        </div>
-        <p className="text-gray-700">{content}</p>
-      </div>
+          <div className="flex flex-col grow">
+            <p className="text-sm">@{user.userName}</p>
+            <p className="font-gray-800">{content}</p>
+            <div className="flex gap-2 mt-1 text-xs text-gray-500">
+              <time>{timeAgo(createdAt)}</time>
+              <div
+                role="button"
+                onClick={() =>
+                  setReplyComment({
+                    id: commentId,
+                    writer: user.userName,
+                  })
+                }
+              >
+                답글 달기
+              </div>
+            </div>
 
-      {/* modal */}
-      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
-        <ModalContent>
-          <ModalHeader>댓글을 삭제하시겠습니까?</ModalHeader>
-          <ModalFooter>
-            <Button variant="light" onPress={onClose}>
-              아니요
-            </Button>
-            <Button
-              color="primary"
-              isLoading={isPending}
-              onPress={() => mutate({ commentId })}
-            >
-              {isPending || '네, 삭제할게요'}
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+            {hasChildren && (
+              <>
+                <div
+                  role="button"
+                  className="flex items-center mt-4 mb-2 text-sm text-gray-700"
+                  onClick={() => setIsShowReply((v) => !v)}
+                >
+                  {/* divider */}
+                  <div className="w-8 h-px bg-divider" />
+                  {isShowReply ? (
+                    <span className="ml-2">답글 숨기기</span>
+                  ) : (
+                    <span className="ml-2">답글 {_count.children}개</span>
+                  )}
+                </div>
+                {isShowReply && (
+                  <WeppCommentReplies
+                    commentId={commentId}
+                    show={isShowReply}
+                    isDeletable={isDeletable}
+                  />
+                )}
+              </>
+            )}
+          </div>
+        </div>
+
+        {isDeletable && <WeppCommentDeleteButton commentId={commentId} />}
+      </div>
     </>
   );
 };
