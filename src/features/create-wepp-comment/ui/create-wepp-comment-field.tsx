ce9d8bfa2c1b42ec3069adb797215e59014fa985
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-08-08 03:51:09 +0900

    feat: replay target ui

--- src/features/create-wepp-comment/ui/create-wepp-comment-field.tsx
+++ src/features/create-wepp-comment/ui/create-wepp-comment-field.tsx
@@ -3,6 +3,8 @@ import { useCreateWeppComment } from '../api/create-wepp-comment';
 import { useForm } from 'react-hook-form';
 import { FormProvider, RHFInput } from '@/shared/ui/hook-form';
 import { Button } from '@nextui-org/react';
+import { useReplyStateStore } from '../lib';
+import { IComment } from '@/shared/types';
 
 const CreateWeppCommentField = () => {
   const { mutate, isPending } = useCreateWeppComment();
@@ -11,14 +13,37 @@ const CreateWeppCommentField = () => {
 
   const { handleSubmit } = methods;
 
-  const onSubmit = (data: { content: string }) => {
+  // reply state
+  const {
+    //state
+    replyComment,
+    // action
+    clearReplyComment,
+  } = useReplyStateStore();
+
+  const onSubmit = (data: Pick<IComment, 'content' | 'parentId'>) => {
+    if (replyComment) {
+      data.parentId = replyComment.id;
+      clearReplyComment();
+    }
+
     mutate(data);
   };
 
   return (
     <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
-      <div className="flex gap-4 mt-4">
-        <RHFInput name="content" placeholder="댓글을 입력해주세요." />
+      <div className="flex gap-4 items-end mt-4">
+        <div className="flex flex-col gap-1 grow">
+          {replyComment && (
+            <div className="flex gap-2 text-sm text-gray-500">
+              To. {replyComment.writer}
+              <div role="button" onClick={clearReplyComment}>
+                취소
+              </div>
+            </div>
+          )}
+          <RHFInput name="content" placeholder="댓글을 입력해주세요." />
+        </div>
         <Button
           type="submit"
           color="primary"
