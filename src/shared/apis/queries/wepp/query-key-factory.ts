f5b72ae90bcb155098fc32f0976b6e9de0480425
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-08-08 03:51:37 +0900

    refactor: Update comment queryKey for better consistency

--- src/shared/apis/queries/wepp/query-key-factory.ts
+++ src/shared/apis/queries/wepp/query-key-factory.ts
@@ -4,5 +4,4 @@ export const weppKeys = {
   all: [PATH_API.WEPP.ROOT] as const,
   detail: (weppId: string) => [...weppKeys.all, weppId] as const,
   mine: [PATH_API.WEPP.MINE] as const,
-  comments: (weppId: string) => [PATH_API.COMMENT.ROOT, weppId] as const,
 };
--- src/views/wepp-detail/api/query-key-factory.ts
+++ src/views/wepp-detail/api/query-key-factory.ts
@@ -3,3 +3,9 @@ import { PATH_API } from '@/shared/apis/path';
 export const likeKeys = {
   hasLiked: (weppId: string) => [PATH_API.LIKE.HAS_LIKED, weppId] as const,
 };
+
+export const commentKeys = {
+  list: (weppId: string) => [PATH_API.COMMENT.ROOT, weppId] as const,
+  replies: (commentId: number) =>
+    [PATH_API.COMMENT.REPLIES, commentId] as const,
+};
