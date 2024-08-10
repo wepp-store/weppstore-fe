d29315ffb2b59eedbdd37d1e7925c451ad3a23e8
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-08-11 02:30:40 +0900

    feat: refetch mine wepp list after deleting wepp

--- src/features/delete-wepp/api/delete-wepp.ts
+++ src/features/delete-wepp/api/delete-wepp.ts
@@ -1,17 +1,23 @@
 'use client';
 
-import { UseMutationOptions, useMutation } from '@tanstack/react-query';
+import {
+  useMutation,
+  useQueryClient,
+  UseMutationOptions,
+} from '@tanstack/react-query';
 import { useParams, useRouter } from 'next/navigation';
 import { axiosInstance } from '@/shared/apis/axios';
 import { PATH_API } from '@/shared/apis/path';
 import { PATH } from '@/shared/constants';
 import toast from 'react-hot-toast';
+import { weppKeys } from '@/shared/apis/queries/wepp';
 
 export const useDeleteWepp = <T>(
   options?: Omit<UseMutationOptions<any, any>, 'mutationKey'>
 ) => {
   const { replace } = useRouter();
   const { weppId }: { weppId: string } = useParams();
+  const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: async () => {
@@ -22,6 +28,7 @@ export const useDeleteWepp = <T>(
     },
     onSuccess: () => {
       toast.success('앱 삭제가 완료되었습니다.');
+      queryClient.invalidateQueries({ queryKey: weppKeys.mine });
       replace(PATH.DEVELOPER.WEPP);
     },
     onError: (error) => {
