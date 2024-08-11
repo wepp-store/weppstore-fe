fde0fccb42e4e2cfe277c734e6b3eafb69614b8c
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-08-12 01:59:43 +0900

    feat: Add metadata for Make PWA page

--- src/app/developer/make-pwa/layout.tsx
+++ src/app/developer/make-pwa/layout.tsx
@@ -1,5 +1,12 @@
 import { DeveloperLayout } from '@/shared/layouts/developer';
+import { Metadata } from 'next';
 import React from 'react';
+
+export const metadata: Metadata = {
+  title: 'Make PWA',
+  description:
+    'Create and customize your PWA manifest and service worker with ease.',
+};
 
 const Layout = ({ children }: React.PropsWithChildren) => {
   return <DeveloperLayout>{children}</DeveloperLayout>;
