1f95870a35e62a407f04f4cb5872449bf89ecf97
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-08-13 13:10:10 +0900

    feat: add query key of oauth login

--- src/shared/apis/queries/auth/query-key-factory.ts
+++ src/shared/apis/queries/auth/query-key-factory.ts
@@ -1,6 +1,8 @@
 import { PATH_API } from '../../path';
+import { type OAuthProvider } from '../../types';
 
 export const authKeys = {
   session: ['my_session'] as const,
   profile: [PATH_API.AUTH.ME] as const,
+  oauthSignIn: (provider: OAuthProvider) => ['oauth-login', provider] as const,
 };
