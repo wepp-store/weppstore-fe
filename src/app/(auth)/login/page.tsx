a9dc0e8a057a7a060b1afbf495c5cba9648ca7ae
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-08-13 14:13:22 +0900

    feat: Update login and sign-up page metadata and move file

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,6 +1,12 @@
 import { LoginPage } from '@/views/login';
 import { CircularProgress } from '@nextui-org/react';
+import { Metadata } from 'next';
 import { Suspense } from 'react';
+
+export const metadata: Metadata = {
+  title: '로그인',
+  description: 'Wepp Store 로그인 페이지',
+};
 
 const SignIn = () => {
   return (
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,4 +1,10 @@
 import { SignUpForm } from '@/features/auth';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '회원가입',
+  description: 'Wepp Store 회원가입 페이지',
+};
 
 const SignUpPage = () => {
   return (
