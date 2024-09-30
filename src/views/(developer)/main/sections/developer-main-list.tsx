420a2232adcf9bd76776cdf97f45cf7d08b20c4c
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-09-30 22:37:13 +0900

    refactor: Optimize rendering logic in developer-main-list.tsx

--- src/views/(developer)/main/sections/developer-main-list.tsx
+++ src/views/(developer)/main/sections/developer-main-list.tsx
@@ -1,8 +1,9 @@
 'use client';
-import { useMineWeppList } from '@/shared/apis/queries/wepp';
-import { Section } from '@/shared/ui/section';
-import { useRouter } from 'next/navigation';
+
 import React from 'react';
+import { useRouter } from 'next/navigation';
+import { Section } from '@/shared/ui/section';
+import { useMineWeppList } from '@/shared/apis/queries/wepp';
 import { DeveloperWeppCard } from '../ui';
 
 const DeveloperMainList = () => {
@@ -17,27 +18,26 @@ const DeveloperMainList = () => {
     fetchNextPage,
   } = useMineWeppList();
 
-  const isNotData = !isLoading && !data?.pages[0]?.data?.length;
+  const isNotData =
+    isFetched && !isLoading && (!data || data.pages[0]?.data?.length === 0);
+
+  if (isNotData) {
+    return (
+      <Section className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+        <>등록된 앱이 없습니다.</>
+      </Section>
+    );
+  }
 
   return (
-    <Section
-      className="
-        grid
-        gap-4
-        grid-cols-1
-        sm:grid-cols-2
-        md:grid-cols-3
-      "
-    >
-      {isNotData && <>등록된 앱이 없습니다.</>}
-      {isFetched &&
-        data?.pages.map((group, i) => (
-          <React.Fragment key={i}>
-            {group.data.map((wepp) => (
-              <DeveloperWeppCard key={wepp.id} wepp={wepp} />
-            ))}
-          </React.Fragment>
-        ))}
+    <Section className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+      {data?.pages.map((group, i) => (
+        <React.Fragment key={i}>
+          {group.data.map((wepp) => (
+            <DeveloperWeppCard key={wepp.id} wepp={wepp} />
+          ))}
+        </React.Fragment>
+      ))}
     </Section>
   );
 };
