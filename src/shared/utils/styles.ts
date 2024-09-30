481c26382295f0a30296fd56c05c7aabc4e660be
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-09-30 22:36:45 +0900

    chore: add annotation in styles.ts

--- src/shared/utils/styles.ts
+++ src/shared/utils/styles.ts
@@ -1,3 +1,10 @@
+/**
+ * @example
+ * import styles from "./styles.module.css";
+ * const cx = bindClassNames(styles);
+ *
+ * <div className={cx("container", "flex", "justify-center")}></div>
+ */
 export const bindClassNames =
   (styles: { [key: string]: string }) =>
   (...classNames: string[]) => {
