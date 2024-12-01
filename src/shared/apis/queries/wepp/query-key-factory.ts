3541362fd67fd8f46d4a1928c4f13ba41636dac0
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-12-01 16:33:05 +0900

    refactor(wepp): Rename mine query key to mineList and update mine function to use mineList for improved clarity and consistency

--- src/shared/apis/queries/wepp/query-key-factory.ts
+++ src/shared/apis/queries/wepp/query-key-factory.ts
@@ -3,5 +3,6 @@ import { PATH_API } from '../../path';
 export const weppKeys = {
   all: [PATH_API.WEPP.ROOT] as const,
   detail: (weppId: string) => [...weppKeys.all, weppId] as const,
-  mine: [PATH_API.WEPP.MINE] as const,
+  mineList: [PATH_API.WEPP.MINE_LIST] as const,
+  mine: (weppId: string) => [...weppKeys.mineList, weppId] as const,
 };
