81fbf873c17a4f3263638bf68a2cad69f7081ad8
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-02-15 18:41:53 +0900

    feat(page): Add notFound handling for missing wepp data in WeppInfoPage

--- src/app/developer/wepp/[weppId]/info/page.tsx
+++ src/app/developer/wepp/[weppId]/info/page.tsx
@@ -1,8 +1,9 @@
 import { getServerQueryClient } from '@/shared/apis/get-query-client';
-import { weppMineDetailOptions } from '@/shared/apis/queries/wepp';
+import { weppKeys, weppMineDetailOptions } from '@/shared/apis/queries/wepp';
 import { WeppInfoPage } from '@/views/(developer)/wepp-info';
 import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
 import { cookies } from 'next/headers';
+import { notFound } from 'next/navigation';
 
 const Page = async ({ params }: { params: { weppId: string } }) => {
   const queryClient = getServerQueryClient();
@@ -16,6 +17,12 @@ const Page = async ({ params }: { params: { weppId: string } }) => {
       headers: token ? { Authorization: `Bearer ${token}` } : {},
     })
   );
+
+  const wepp = queryClient.getQueryData(weppKeys.mine(params.weppId));
+
+  if (!wepp) {
+    notFound();
+  }
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
