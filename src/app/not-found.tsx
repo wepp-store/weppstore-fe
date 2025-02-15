10403cb3c4b59fb97cb46f235100467c451fa538
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-02-15 18:42:02 +0900

    feat(page): Implement custom 404 Not Found pages for end-user and developer sections

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,16 +1,16 @@
-import Link from 'next/link';
-
 const NotFound = () => {
   return (
     <div className="size-full flex flex-col items-center justify-center">
       <h1 className="text-9xl font-bold text-blue-500">404</h1>
-      <p className="text-gray-600 mt-4 mb-8">페이지가 존재하지 않습니다.</p>
-      <Link
+      <p className="text-gray-600 mt-4 mb-8">
+        페이지가 존재하지 않거나 접근 권한이 없습니다.
+      </p>
+      <a
         href="/"
         className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
       >
         홈으로 이동하기
-      </Link>
+      </a>
     </div>
   );
 };
