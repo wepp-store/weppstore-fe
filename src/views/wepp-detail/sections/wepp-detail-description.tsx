54a34024de61d9705621c910c16b321daeeec92c
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-03-30 20:09:46 +0900

    feat(wepp-detail): add compatibility chips for mobile, tablet, and desktop devices

--- src/views/wepp-detail/sections/wepp-detail-description.tsx
+++ src/views/wepp-detail/sections/wepp-detail-description.tsx
@@ -1,5 +1,6 @@
 import { IWepp } from '@/shared/types';
 import { Section } from '@/shared/ui/section';
+import { Chip } from '@nextui-org/react';
 import React from 'react';
 
 interface Props {
@@ -7,12 +8,22 @@ interface Props {
 }
 
 const WeppDetailDescription = ({ wepp }: Props) => {
-  const { description } = wepp || {};
+  const { description, isMobile, isTablet, isDesktop } = wepp || {};
 
   return (
     <Section>
-      <h3 className="text-lg font-semibold mb-2">앱 설명</h3>
-      <p className="text-gray-700 mb-4">{wepp?.description}</p>
+      {/* <h3 className="text-lg font-semibold mb-2">앱 설명</h3> */}
+      <p className="text-gray-700 mb-4">{description}</p>
+
+      <div className="mt-8 flex gap-4 items-center">
+        {isMobile && (
+          <Chip className="bg-gray-200 rounded-md">#모바일 호환</Chip>
+        )}
+        {isTablet && (
+          <Chip className="bg-gray-200 rounded-md">#태블릿 호환</Chip>
+        )}
+        {isDesktop && <Chip className="bg-gray-200 rounded-md">#PC 호환</Chip>}
+      </div>
     </Section>
   );
 };
