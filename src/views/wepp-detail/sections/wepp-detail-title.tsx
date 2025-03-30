4bc747c9627733c6c75ad39191e2e77879de4d03
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-03-30 20:09:53 +0900

    feat(wepp-detail): enhance title section with category display and comment functionality

--- src/views/wepp-detail/sections/wepp-detail-title.tsx
+++ src/views/wepp-detail/sections/wepp-detail-title.tsx
@@ -2,20 +2,31 @@ import { IWepp } from '@/shared/types';
 import { Section } from '@/shared/ui/section';
 import { Image, Tooltip } from '@nextui-org/react';
 import React from 'react';
-import { ShieldCheck, ShieldOff } from 'lucide-react';
+import { Heart, MessageCircle, ShieldCheck, ShieldOff } from 'lucide-react';
 import { WeppLikeButton } from '../ui';
 import WeppInstallButton from '../ui/wepp-install-button';
+import { ELEMENT_ID } from '@/shared/constants';
 
 interface Props {
   wepp: IWepp | undefined;
 }
 
 const WeppDetailTitle = ({ wepp }: Props) => {
-  const { logo, name, isVerified, tagLine } = wepp || {};
+  const { logo, name, isVerified, tagLine, categories, _count } = wepp || {};
+
+  const onCommentIconClick = () => {
+    const inputElement = document.getElementById(
+      ELEMENT_ID.CREATE_COMMENT_FIELD
+    );
+    inputElement?.scrollIntoView({
+      behavior: 'smooth',
+    });
+    inputElement?.focus();
+  };
 
   return (
     <Section className="flex flex-col gap-4">
-      <div className="flex items-center mb-6 gap-8">
+      <div className="flex items-center mb-3 gap-8">
         <Image
           src={logo || undefined}
           width={96}
@@ -46,15 +57,52 @@ const WeppDetailTitle = ({ wepp }: Props) => {
             )}
           </h2>
 
-          <p className="text-gray-600">{tagLine}</p>
-
-          <div className="flex justify-between items-center min-w-48">
-            <WeppLikeButton />
-            <WeppInstallButton wepp={wepp} className="hidden md:flex" />
-          </div>
+          <p className="text-gray-500">{tagLine}</p>
         </div>
       </div>
-      <WeppInstallButton wepp={wepp} className="md:hidden" />
+
+      <div className="flex justify-between items-center min-w-48">
+        <div className="flex items-center gap-2 text-gray-600">
+          <span className="">
+            {(categories ?? [])
+              .slice(0, 2)
+              .map((c) => c.name)
+              .join(', ')}
+          </span>
+
+          {(categories ?? []).length > 2 && (
+            <span className="text-gray-500">
+              외 {(categories ?? []).length - 2}개
+            </span>
+          )}
+
+          <span className="font-bold">·</span>
+
+          <div className="flex gap-3 text-gray-600">
+            <span
+              className="flex items-center gap-1 text-gray-500"
+              aria-label="좋아요 수"
+            >
+              <WeppLikeButton />
+              {_count?.likes || 0}
+            </span>
+            <span
+              className="flex items-center gap-1 text-gray-500"
+              aria-label="댓글 수"
+            >
+              <MessageCircle
+                size={20}
+                onClick={onCommentIconClick}
+                role="button"
+                aria-label="댓글 달기"
+              />
+              {_count?.comments || 0}
+            </span>
+          </div>
+        </div>
+
+        <WeppInstallButton wepp={wepp} className="ml-auto rounded-md" />
+      </div>
     </Section>
   );
 };
