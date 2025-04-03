ebf61b35d88c9fadeb9bf77acf33f1d8abbcea21
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-04-03 21:53:51 +0900

    feat(profile): enhance UpdateProfileImage component

--- src/features/update-profile/ui/update-profile-image.tsx
+++ src/features/update-profile/ui/update-profile-image.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import { Image, Tooltip } from '@nextui-org/react';
 import { useUploadProfileImage } from '../api/upload-profile-image';
 
-const UpdateProfileImage = ({ src }: { src: string | null | undefined }) => {
+const UpdateProfileImage = ({
+  src,
+  isMine,
+}: {
+  src: string | null | undefined;
+  isMine: boolean;
+}) => {
   const { mutate, isPending } = useUploadProfileImage();
 
   const onUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -11,6 +17,31 @@ const UpdateProfileImage = ({ src }: { src: string | null | undefined }) => {
 
     mutate(files[0]);
   };
+
+  if (!isMine) {
+    return (
+      <Image
+        width={128}
+        height={128}
+        src={src || '/no-image.svg'}
+        alt="Profile"
+        fallbackSrc="/no-image.svg"
+        className="
+          w-24 md:w-32
+          min-w-24 md:min-w-32
+          max-w-24 md:max-w-32
+          h-24 md:h-32
+          min-h-24 md:min-h-32
+          max-h-24 md:max-h-32
+          rounded-full
+          border-4
+          border-white
+          z-50
+          cursor-pointer
+          "
+      />
+    );
+  }
 
   return (
     <Tooltip showArrow content="프로필 이미지 수정">
--- src/views/profile/index.ts
+++ src/views/profile/index.ts
@@ -1,1 +1,2 @@
 export { default as ProfilePage } from './profile-page';
+export { default as OtherProfilePage } from './other-profile-page';
