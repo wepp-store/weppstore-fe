879f91e27681d05b3047733dbdb0c2c193bfa35b
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-04-19 18:44:08 +0900

    feat: add eventLogs key for constructing event log query

--- src/shared/apis/queries/wepp/query-key-factory.ts
+++ src/shared/apis/queries/wepp/query-key-factory.ts
@@ -5,4 +5,6 @@ export const weppKeys = {
   detail: (weppId: string) => [...weppKeys.all, weppId] as const,
   mineList: [PATH_API.WEPP.MINE_LIST] as const,
   mine: (weppId: string) => [...weppKeys.mineList, weppId] as const,
+  eventLogs: (weppId: string, from?: string, to?: string) =>
+    [...weppKeys.all, PATH_API.WEPP.EVENT_LOG, weppId, from, to] as const,
 };
